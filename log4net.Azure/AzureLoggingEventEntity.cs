3dc552bde9fcc0d163a46233bba5fce9d87f095e
Stephen Chung <Stephen.Chung@intexact.com>
2018-03-11 18:24:47 +0800

    Remove fields that don't make sense in .NET Standard.

--- log4net.Azure/AzureLoggingEventEntity.cs
+++ log4net.Azure/AzureLoggingEventEntity.cs
@@ -10,36 +10,40 @@ namespace log4net.Appender
 	{
 		public AzureLoggingEventEntity (LoggingEvent e, PartitionKeyTypeEnum partitionKeyType)
 		{
-			Domain = e.Domain;
-			Identity = e.Identity;
+			// NOTE - Remove fields that don't make sense in .NET Standard
+			//Domain = e.Domain;
+			//Identity = e.Identity;
 			Level = e.Level.ToString();
-			var sb = new StringBuilder(e.Properties.Count);
-			foreach (DictionaryEntry entry in e.Properties) {
-				sb.AppendFormat("{0}:{1}", entry.Key, entry.Value);
-				sb.AppendLine();
+
+			if (e.Properties != null && e.Properties.Count > 0) {
+				var sb = new StringBuilder();
+				foreach (DictionaryEntry entry in e.Properties) {
+					sb.AppendFormat("{0}:{1}", entry.Key, entry.Value);
+					sb.AppendLine();
+				}
+				Properties = sb.ToString();
 			}
-			Properties = sb.ToString();
-			Message = e.RenderedMessage + Environment.NewLine + e.GetExceptionString();
+
+			var exception = e.GetExceptionString();
+			Message = string.IsNullOrWhiteSpace(exception) ? e.RenderedMessage : e.RenderedMessage + Environment.NewLine + exception;
 			ThreadName = e.ThreadName;
-			EventTimeStamp = e.TimeStamp;
-			UserName = e.UserName;
-			Location = e.LocationInformation.FullInfo;
-			ClassName = e.LocationInformation.ClassName;
-			FileName = e.LocationInformation.FileName;
-			LineNumber = e.LocationInformation.LineNumber;
-			MethodName = e.LocationInformation.MethodName;
+			EventTimeStamp = e.TimeStampUtc;
+			//UserName = e.UserName;
+			//Location = e.LocationInformation.FullInfo;
+			//ClassName = e.LocationInformation.ClassName;
+			//FileName = e.LocationInformation.FileName;
+			//LineNumber = e.LocationInformation.LineNumber;
+			//MethodName = e.LocationInformation.MethodName;
 			// TODO - No stack frames for .NET Standard?
 			//StackFrames = e.LocationInformation.StackFrames;
 
-			if (e.ExceptionObject != null) {
-				Exception = e.ExceptionObject.ToString();
-			}
+			if (e.ExceptionObject != null) Exception = e.ExceptionObject.ToString();
 
 			PartitionKey = e.MakePartitionKey(partitionKeyType);
 			RowKey = e.MakeRowKey();
 		}
 
-		public string UserName { get; set; }
+		//public string UserName { get; set; }
 
 		public DateTime EventTimeStamp { get; set; }
 
@@ -51,22 +55,22 @@ namespace log4net.Appender
 
 		public string Level { get; set; }
 
-		public string Identity { get; set; }
+		//public string Identity { get; set; }
 
-		public string Domain { get; set; }
+		//public string Domain { get; set; }
 
-		public string Location { get; set; }
+		//public string Location { get; set; }
 
 		public string Exception { get; set; }
 
-		public string ClassName { get; set; }
+		//public string ClassName { get; set; }
 
-		public string FileName { get; set; }
+		//public string FileName { get; set; }
 
-		public string LineNumber { get; set; }
+		//public string LineNumber { get; set; }
 
-		public string MethodName { get; set; }
+		//public string MethodName { get; set; }
 
-		public StackFrameItem[] StackFrames { get; set; }
+		//public StackFrameItem[] StackFrames { get; set; }
 	}
 }
