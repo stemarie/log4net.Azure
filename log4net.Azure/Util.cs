52a61ede4ab84dcdbcc8ccbe437d86bf1dd4f8f1
shahjeet297 <shahjeet297@gmail.com>
2018-12-18 16:36:26 +0530

    Remove dependency from Cloud Configuration Manager

--- log4net.Azure/Util.cs
+++ log4net.Azure/Util.cs
@@ -27,11 +27,11 @@ namespace log4net.Appender
             }
 
             // Fallback to CloudConfigurationManager in case we're running as a worker/web role
-            var azConfig = CloudConfigurationManager.GetSetting(connectionStringName);
-            if (!string.IsNullOrWhiteSpace(azConfig))
-            {
-                return azConfig;
-            }
+            // var azConfig = CloudConfigurationManager.GetSetting(connectionStringName);
+            // if (!string.IsNullOrWhiteSpace(azConfig))
+            // {
+            //    return azConfig;
+            // }
 
             // Connection string not found, throw exception to notify the user
             throw new ApplicationException(Resources.AzureConnectionStringNotSpecified);
